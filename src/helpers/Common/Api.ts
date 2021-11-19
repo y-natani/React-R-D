2be6b18211eef8346c041d60424ef2d2717ae912
y-natani <y-natani@yukoyuko.co.jp>
2021-11-19 16:36:11 +0900

    comment2

--- src/helpers/Common/Api.ts
+++ src/helpers/Common/Api.ts
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import { toQs } from './String'
 
+// Comment1
+// Comment2
 export type Param = { [key: string]: any }
 
 export const fetcher = async (url: string, param: Param = {}) => {
